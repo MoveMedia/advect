a45ea7f358d188fab800848ee7bb77b0680dec55
PartyGodTroy <kidveno@live.com>
2025-01-30 18:45:58 -0500

    exhaustive?

--- advect.org/src/data/docs.ts
+++ advect.org/src/data/docs.ts
@@ -6,7 +6,7 @@ export interface DocPage {
     hide?:boolean
 }
 export const docPages = Object
-    .values(await import.meta.glob("../pages/docs/**/*.astro", { eager:true })) as {
+    .values(await import.meta.glob("../pages/docs/**/*.astro", { eager:true, exhaustive:true  })) as {
         pageInfo:DocPage,
         default: Function,
         file:string
