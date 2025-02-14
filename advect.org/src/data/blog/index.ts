e730009dc07b155a45dccf33b58a7a963a76358f
PartyGodTroy <kidveno@live.com>
2025-02-13 22:56:14 -0500

    wow lots of docs

--- advect.org/src/data/blog/index.ts
+++ advect.org/src/data/blog/index.ts
@@ -1,7 +1,10 @@
 import type { Page, SitePage } from "../SitePage"
 
 export interface BlogPage extends Page {
-    blurb?:string
+    blurb?:string,
+    featured_img?:string
+    readtime?:string
+    published?:Date;
 }
 export const blogPages = Object
     .values(await import.meta.glob("./**/*.astro", { eager:true })) as SitePage<BlogPage>[]
